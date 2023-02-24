fa13d5d7d8415174e1b6d8e949e845bd2db73790
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-02-24 16:50:38 +0300

    Задача 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число ");
+int userNumberA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число ");
+int userNumberB = int.Parse(Console.ReadLine() ?? "");
+if (userNumberA > userNumberB)
+{
+    Console.WriteLine($"{userNumberA} больше {userNumberB}");
+}
+else
+{
+    Console.WriteLine($"{userNumberB} больше {userNumberA}");
+}
