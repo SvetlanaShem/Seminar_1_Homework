83a41a02467c51ef972dacd5881d9495edab138a
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-02-24 17:28:09 +0300

    Задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число ");
+int userNumberA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число ");
+int userNumberB = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите третье число ");
+int userNumberC = int.Parse(Console.ReadLine() ?? "");
+int max = userNumberA;
+if ((max < userNumberB) & (userNumberB > userNumberC))
+{
+    max = userNumberB;
+}
+else
+{
+    if (max < userNumberC)
+
+        max = userNumberC;
+}
+Console.WriteLine($"{max} максимальное из трех чисел");
