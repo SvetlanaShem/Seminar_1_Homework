c4252ca70c82ada9cff76d27ad419cb40ad49c1c
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-02-24 18:21:40 +0300

    Задача 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число: ");
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+for (int i = 1; i <= userNumber; i++)
+    if (i % 2 == 0)
+        Console.WriteLine($"{i}");
