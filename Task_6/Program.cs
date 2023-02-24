de7d775203375cde6b816fad561140a01f9b8d7d
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-02-24 17:51:10 +0300

    Задача 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+if (userNumber%2 == 0)
+{
+    Console.WriteLine($"{userNumber} четное число");
+}
+else
+{
+    Console.WriteLine($"{userNumber} нечетное число");
+}
